73fc6db1eeb941249aaee7dc3528654c390e6ad3
상우 <126045264+CCTTBG@users.noreply.github.com>
2025-08-05 22:57:45 +0900

    유저 로그인 로직 개발

--- src/components/loginPage.vue
+++ src/components/loginPage.vue
@@ -2,21 +2,40 @@
     <div>
         <h2>로그인</h2>
         <div>
-            아이디: <input class="userId">
-            비밀번호: <input class="userpass">
+        아이디: <input v-model="userId" class="userId" />
+        비밀번호: <input v-model="userPass" class="userPass" type="password" />
+
         </div>
         <button @click="handleLogin">로그인하기</button>
     </div>
 </template>
 
 <script>
-export default {
+    export default {
+    data() {
+        return {
+        userId: '',
+        userPass: ''
+        }
+    },
     methods: {
-        handleLogin() {
-        // 로그인 성공 시
+        async handleLogin() {
+        const response = await fetch('instarJson/userdata.json')
+        const users = await response.json()
+            console.log(users);
+        const userData = users.find(u => u.userId === this.userId.trim() && u.passWord === this.userPass.trim());
+
+        if (userData) {
             localStorage.setItem('islogin', 'true')
             this.$router.push('/my-info')
+        } else {
+            alert('아이디 또는 비밀번호가 올바르지 않습니다.')
         }
-    } 
-}
+        }
+    }
+    }
 </script>
+
+<style>
+
+</style>
